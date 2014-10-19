9e5987657e17c4ce829b7998699593ec7214a832
Nelsonochoam <nelsonochoam@gmail.com>
2014-10-18 21:36:14 -0600

    Added one unit test for the view.html rendering

--- imagemanagement/tests.py
+++ imagemanagement/tests.py
@@ -1,5 +1,15 @@
 from django.test import TestCase
 
-
 # Create your tests here.
+class TestView(TestCase):
+
+    def test_view(self):
+
+    	resp = self.client.get('/view/')
+    	self.assertEqual(resp.status_code,200)
+    	self.assertTrue('ImgList' in resp.context)
+
+
+
+
 
